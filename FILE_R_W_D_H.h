610283d21093d7bbf418b17e17531fc689e33de7
Yahia Ahmed <70311284+engYahiaAhmed@users.noreply.github.com>
2022-10-10 22:13:17 +0200

    Delete FILE_R_W_D_H.h

--- FILE_R_W_D_H.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_R_W_D_H
-#define FILE_R_W_D_H
-#include"student.h"
-void school_initiReadDataBase(stud_t* school, int classNum);
-void school_WriteFile(stud_t* stud , int classNum);
-void school_Delete_Data_File(int classNum,int id);
-void school_WriteStudentInSpecificFile(stud_t* stud,FILE* classFile);
-void school_editStudentDataFile(int classNum,int id);
-#endif
